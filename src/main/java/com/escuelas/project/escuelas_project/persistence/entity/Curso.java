d8ca3075b8cddca69d76a7cca7e1247b058304cd
Gazpachin33 <emikpo66@gmail.com>
2024-07-16 21:41:25 -0300

    feat(Backend): :bug: Test refactor

--- src/main/java/com/escuelas/project/escuelas_project/persistence/entity/Curso.java
+++ src/main/java/com/escuelas/project/escuelas_project/persistence/entity/Curso.java
@@ -14,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
@@ -24,6 +25,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "cursos")
 public class Curso {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/escuelas/project/escuelas_project/EscuelasProjectApplicationTests.java
+++ src/test/java/com/escuelas/project/escuelas_project/EscuelasProjectApplicationTests.java
@@ -7,11 +7,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.escuelas.project.escuelas_project.custom.CommonTestUtils;
+import com.escuelas.project.escuelas_project.service.models.dtos.escuela.EscuelaResponseDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,6 +25,9 @@ class EscuelasProjectApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	// Post test start
 
@@ -41,7 +47,10 @@ class EscuelasProjectApplicationTests {
 	}
 
 	@Test
+	@Transactional
 	void llamada_a_post_escuelas_sin_nombre_devuelve_error_400_codeError_4() throws Exception {
+		this.crearEscuelas(mockMvc);
+
 		CommonTestUtils.verificarErrorPostDuplicado(mockMvc, "{ \"nombre\": \"test\" }", "/api/v1/escuelas/save", 4,
 				HttpStatus.BAD_REQUEST);
 	}
@@ -58,17 +67,24 @@ class EscuelasProjectApplicationTests {
 	}
 
 	@Test
+	@Transactional
 	void llamada_a_get_escuelas_por_id_devuelve_escuela_activa_200() throws Exception {
+		String id = this.crearEscuelas(mockMvc);
 
-		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/escuelas/find/3").contentType(MediaType.APPLICATION_JSON))
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/escuelas/find/" + id ).contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
 				.andExpect(MockMvcResultMatchers.jsonPath("$.nombre").value("test"));
 	}
 
 	@Test
+	@Transactional
 	void llamada_a_get_escuelas_por_id_devuelve_escuela_inactiva_400() throws Exception {
-		CommonTestUtils.verificarErrorGetSinId(mockMvc, "/api/v1/escuelas/find/1", 2, HttpStatus.BAD_REQUEST);
+		String id = this.crearEscuelas(mockMvc);
+
+		CommonTestUtils.operacionDeleteExitosa(mockMvc, "/api/v1/escuelas/disable/" + id, HttpStatus.OK);
+
+		CommonTestUtils.verificarErrorGetSinId(mockMvc, "/api/v1/escuelas/find/" + id, 2, HttpStatus.BAD_REQUEST);
 	}
 
 	@Test
@@ -100,14 +116,20 @@ class EscuelasProjectApplicationTests {
 	}
 
 	@Test
+	@Transactional
 	void llamada_a_delete_escuelas_id_desactivada_devuelve_error_400() throws Exception {
-		CommonTestUtils.verificarErrorDeleteSinId(mockMvc, "/api/v1/escuelas/disable/1", 2, HttpStatus.BAD_REQUEST);
+		String id = this.crearEscuelas(mockMvc);
+
+		CommonTestUtils.operacionDeleteExitosa(mockMvc, "/api/v1/escuelas/disable/" + id, HttpStatus.OK);
+
+		CommonTestUtils.verificarErrorDeleteSinId(mockMvc, "/api/v1/escuelas/disable/" + id, 2, HttpStatus.BAD_REQUEST);
 	}
 
 	@Transactional
 	@Test
 	void llamada_a_delete_escuelas_id_activada_devuelve_200() throws Exception {
-		CommonTestUtils.operacionDeleteExitosa(mockMvc, "/api/v1/escuelas/disable/3", HttpStatus.OK);
+		String id = this.crearEscuelas(mockMvc);
+		CommonTestUtils.operacionDeleteExitosa(mockMvc, "/api/v1/escuelas/disable/" + id, HttpStatus.OK);
 	}
 
 	// Delete test end
@@ -123,7 +145,7 @@ class EscuelasProjectApplicationTests {
 	@Test
 	@Transactional
 	void llamada_a_put_update_escuelas_sin_contenido_devuelve_400() throws Exception {
-		CommonTestUtils.verificarErrorPut(mockMvc, "" , "/api/v1/escuelas/update/3", 1, HttpStatus.BAD_REQUEST);
+		CommonTestUtils.verificarErrorPut(mockMvc, "" , "/api/v1/escuelas/update/99999", 1, HttpStatus.BAD_REQUEST);
 	}
 
 	@Test
@@ -141,25 +163,35 @@ class EscuelasProjectApplicationTests {
 	@Test
 	@Transactional
 	void llamada_a_put_update_escuelas_id_desactivada_devuelve_error_400() throws Exception {
-		CommonTestUtils.verificarErrorPut(mockMvc, "{ \"nombre\": \"test2 temporal\" }" , "/api/v1/escuelas/update/1", 2, HttpStatus.BAD_REQUEST);
+		String id = this.crearEscuelas(mockMvc);
+
+		CommonTestUtils.operacionDeleteExitosa(mockMvc, "/api/v1/escuelas/disable/" + id, HttpStatus.OK);
+
+		CommonTestUtils.verificarErrorPut(mockMvc, "{ \"nombre\": \"test2 temporal\" }" , "/api/v1/escuelas/update/" + id, 2, HttpStatus.BAD_REQUEST);
 	}
 
 	@Test
 	@Transactional
 	void llamada_a_put_update_escuelas_id_activada_devuelve_200() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/escuelas/update/3")
+		String id = this.crearEscuelas(mockMvc);
+
+		mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/escuelas/update/" + id)
 				.contentType(MediaType.APPLICATION_JSON)
 				.content("{ \"nombre\": \"test2 temporal\" }"))
 				.andExpect(status().isOk())
 				.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
 				.andExpect(MockMvcResultMatchers.jsonPath("$.nombre").value("test2 temporal"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(3));
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(id));
 	}
 
 	@Test
 	@Transactional
 	void llamada_a_put_enable_escuelas_id_enable_devuelve_200() throws Exception {
-		CommonTestUtils.verificarErrorPut(mockMvc, "{}" , "/api/v1/escuelas/enable/1", 0, HttpStatus.OK);
+		String id = this.crearEscuelas(mockMvc);
+
+		CommonTestUtils.operacionDeleteExitosa(mockMvc, "/api/v1/escuelas/disable/" + id, HttpStatus.OK);
+
+		CommonTestUtils.verificarErrorPut(mockMvc, "{}" , "/api/v1/escuelas/enable/" + id, 0, HttpStatus.OK);
 	}
 
 	@Test
@@ -183,10 +215,25 @@ class EscuelasProjectApplicationTests {
 	@Test
 	@Transactional
 	void llamada_a_put_enable_escuelas_id_enable_devuelve_error_400() throws Exception {
-		CommonTestUtils.verificarErrorPut(mockMvc, "{}" , "/api/v1/escuelas/enable/3", 2, HttpStatus.BAD_REQUEST);
+		String id = this.crearEscuelas(mockMvc);
+
+		CommonTestUtils.verificarErrorPut(mockMvc, "{}" , "/api/v1/escuelas/enable/" + id, 2, HttpStatus.BAD_REQUEST);
 	}
-
-
 	// Put test end
+
+
+	private String crearEscuelas( MockMvc mockMvc) throws Exception {
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/escuelas/save").contentType(MediaType.APPLICATION_JSON)
+		.content("{ \"nombre\": \"test\" }")).andExpect(status().isCreated())
+		.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.nombre").value("test"))
+		.andReturn();
+
+		String content = result.getResponse().getContentAsString();
+
+		EscuelaResponseDto escuelaResponseDto = objectMapper.readValue(content, EscuelaResponseDto.class);
+		
+		return escuelaResponseDto.id().toString();
+	}
 
 }
