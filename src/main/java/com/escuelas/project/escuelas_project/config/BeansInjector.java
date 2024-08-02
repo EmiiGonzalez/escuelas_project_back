25c4900cd5fafe68a546e04d5491be16ead27515
Emiliano Gonzalez <emigonzalez3333@gmail.com>
2024-08-02 06:35:53 -0300

    feat(Backend): :fire: Se agrega la enviroment cors allowed al docker para configurar el despliuegue de la app

--- src/main/java/com/escuelas/project/escuelas_project/config/BeansInjector.java
+++ src/main/java/com/escuelas/project/escuelas_project/config/BeansInjector.java
@@ -1,5 +1,6 @@
 package com.escuelas.project.escuelas_project.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -7,11 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class BeansInjector implements WebMvcConfigurer{
+public class BeansInjector implements WebMvcConfigurer {
+
+    @Value("${CORS_ALLOWED_ORIGINS}")
+    private String corsAllowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-        .allowedOrigins("*")
-        .allowedMethods("GET", "POST", "PUT", "DELETE");
+        String[] origins = corsAllowedOrigins.split(",");
+
+        registry.addMapping("/**") // Permite cualquier ruta
+                .allowedOrigins(origins) // Orígenes específicos permitidos
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
