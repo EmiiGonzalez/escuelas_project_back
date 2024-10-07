57df9dc5c97a500401261212adc47753e51ac06d
Emiliano Gonzalez <emigonzalez3333@gmail.com>
2024-10-07 12:24:26 -0300

    refactor(Back): :fire: Remove innecesary data

--- src/main/java/com/escuelas/project/escuelas_project/asistencia/entities/AsistenciaResponsePorClaseDto.java
+++ src/main/java/com/escuelas/project/escuelas_project/asistencia/entities/AsistenciaResponsePorClaseDto.java
@@ -2,13 +2,10 @@ package com.escuelas.project.escuelas_project.asistencia.entities;
 
 public record AsistenciaResponsePorClaseDto(
     Long id,
-    String contenido,
-    String fecha,
     Boolean asistio,
     String alumno
 ) {
     public AsistenciaResponsePorClaseDto(Asistencia asistencia) {
-        this(asistencia.getId_asistecia(), asistencia.getClase().getContenido(), asistencia.getClase().getFechaString(),
-                asistencia.getAsistio(), asistencia.getAlumno().getNombre());
+        this( asistencia.getId_asistecia(), asistencia.getAsistio(), asistencia.getAlumno().getNombre() );
     }
 }
