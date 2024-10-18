f2ccf22e29595fec6514b9d32e95dc7a2033584f
Emiliano Gonzalez <emigonzalez3333@gmail.com>
2024-10-18 02:40:16 -0300

    feat(Back): :zap: Clase returns if it have a asistencia records

--- src/main/java/com/escuelas/project/escuelas_project/clase/entities/Clase.java
+++ src/main/java/com/escuelas/project/escuelas_project/clase/entities/Clase.java
@@ -1,16 +1,22 @@
 package com.escuelas.project.escuelas_project.clase.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.escuelas.project.escuelas_project.asistencia.entities.Asistencia;
 import com.escuelas.project.escuelas_project.curso.entities.Curso;
 import com.escuelas.project.escuelas_project.service.utils.Util;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -42,6 +48,9 @@ public class Clase {
     @JoinColumn(name = "id_curso", nullable = false)
     private Curso curso;
 
+    @OneToMany(mappedBy = "clase", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Asistencia> asistencias;
+
 
     public Clase(Curso curso, ClaseDto dto) {
         this.curso = curso;
@@ -49,6 +58,7 @@ public class Clase {
         this.contenido = dto.contenido();
         this.numero_de_clase = dto.numeroDeClase();
         this.activo = true;
+        this.asistencias = new ArrayList<Asistencia>();
     }
 
     public void update(ClaseUpdateDto dto) {
--- src/main/java/com/escuelas/project/escuelas_project/clase/entities/ClaseResponseDto.java
+++ src/main/java/com/escuelas/project/escuelas_project/clase/entities/ClaseResponseDto.java
@@ -4,9 +4,10 @@ public record ClaseResponseDto(
     Long id,
     String fecha,
     String contenido,
-    Integer numeroDeClase
+    Integer numeroDeClase,
+    Boolean asistencia
 ) {
     public ClaseResponseDto(Clase clase) {
-        this(clase.getId_clase(), clase.getFechaString(), clase.getContenido(), clase.getNumero_de_clase());
+        this(clase.getId_clase(), clase.getFechaString(), clase.getContenido(), clase.getNumero_de_clase(), clase.getAsistencias().size() > 0);
     }
 }
