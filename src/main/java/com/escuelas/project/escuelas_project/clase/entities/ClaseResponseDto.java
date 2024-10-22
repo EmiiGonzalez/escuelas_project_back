baf432d80f5368b73b0c693fc0299be5de7eb4a8
Emiliano Gonzalez <emigonzalez3333@gmail.com>
2024-10-22 01:14:30 -0300

    feat(Back): :zap: Dto response id curso now

--- src/main/java/com/escuelas/project/escuelas_project/clase/entities/ClaseResponseDto.java
+++ src/main/java/com/escuelas/project/escuelas_project/clase/entities/ClaseResponseDto.java
@@ -5,9 +5,10 @@ public record ClaseResponseDto(
     String fecha,
     String contenido,
     Integer numeroDeClase,
-    Boolean asistencia
+    Boolean asistencia,
+    Long idCurso
 ) {
     public ClaseResponseDto(Clase clase) {
-        this(clase.getId_clase(), clase.getFechaString(), clase.getContenido(), clase.getNumero_de_clase(), clase.getAsistencias().size() > 0);
+        this(clase.getId_clase(), clase.getFechaString(), clase.getContenido(), clase.getNumero_de_clase(), clase.getAsistencias().size() > 0, clase.getCurso().getId_curso());
     }
 }
